fe89931336c4b1838241cd4a93023020b94778b9
vatsal301 <vatsalgajjar8623@gmali.com>
2023-12-15 18:30:02 +0530

    Number Guessing Game start

--- Programs/Preksha/AgeCal/ageCal.js
+++ Programs/Preksha/AgeCal/ageCal.js
@@ -1,14 +1,14 @@
 function click_kar(){
     let DOB = document.getElementById("dob").value;
 
-    // console.log(DOB);
-    let dt =new Date(DOB);
-    console.log(dt);
-    let GetYear = dt.getFullYear();
-    console.log(GetYear);
+    console.log(DOB); // 23-10-2002
+    let dt = new Date(DOB); 
+    console.log(dt); // dt.getFUllYear(); -- 2002
+    let userBYear = dt.getFullYear();
+    console.log(userBYear);
     //ALT + SHIFT + DOWN ARROW
     
     // console.log(dt.getFullYear());
-    let current_age = 2023 - GetYear;
+    let current_age = 2023 - userBYear;
     console.log(current_age);
 }
